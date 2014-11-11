7451fb88bdd3906686d96d550b1f88edba737f1e
Tim Hartmann <tfhartmann@gmail.com>
2014-11-10 20:18:27 -0500

    Finished lvl 2

--- rubywarrior/paxindustria-beginner/player.rb
+++ rubywarrior/paxindustria-beginner/player.rb
@@ -1,6 +1,23 @@
 class Player
   def play_turn(warrior)
     # add your code here
-    warrior.walk!
+    @space = warrior.feel
+    puts @space
+    if warrior.feel.enemy?
+      warrior.attack!
+    end
+    if warrior.feel.empty?
+      warrior.walk!
+    end
   end
 end
+
+#Warrior Abilities:
+#  warrior.feel
+#    Returns a Space for the given direction (forward by default).
+#
+#  warrior.attack!
+#    Attacks a unit in given direction (forward by default).
+#
+#  warrior.walk!
+#    Move in the given direction (forward by default).
